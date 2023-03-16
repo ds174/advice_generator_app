1a6eeb37680e1ed366e35957777ec6fe48a24bb3
ds174 <110696419+ds174@users.noreply.github.com>
2023-03-16 03:07:17 +0200

    final

--- script.js
+++ script.js
@@ -3,20 +3,25 @@ const advice_id = document.getElementById("advice_id");
 const advice = document.getElementById("advice");
 
 const options = {
-    method: "GET"
+  method: "GET",
 };
 
 const apiURL = "https://api.adviceslip.com/advice";
 
-
-
 async function getAdvice() {
-  const response = await fetch (apiURL, options);
+  const response = await fetch(apiURL, options);
   const data = await response.json();
 
-
-  advice_id.innerText = ("Advice # " + data.slip.id);
-  advice.innerText =('"' + data.slip.advice + '"');
+  advice_id.innerText = "Advice # " + data.slip.id;
+  function first() {
+    if (data.slip.advice.length > 100) {
+      var change = document.getElementById("advice");
+      change.style.fontSize = "1.4rem";
+      advice.innerText = '"' + data.slip.advice + '"';
+    } else {
+      advice.innerText = '"' + data.slip.advice + '"';
+    }
+  }
+  first();
 }
-
 btnEl.addEventListener("click", getAdvice);
